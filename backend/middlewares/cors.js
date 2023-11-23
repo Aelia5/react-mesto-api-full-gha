65e5b8286a9b4315e0ae1113afa00a8aa6841e21
Olga Liubimova <lov800@yandex.ru>
2023-11-23 11:36:07 +0300

    Настроить обработку CORS(5.2)

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,22 +3,22 @@ const allowedCors = [
   'http://aelia.students.nomoredomainsmonster.ru',
   'https://api.aelia.students.nomoredomainsmonster.ru',
   'http://api.aelia.students.nomoredomainsmonster.ru',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 module.exports.cors = (req, res, next) => {
-  const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { method } = req;
+  const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
     }
-    return res.end();
   }
   return next();
 };
