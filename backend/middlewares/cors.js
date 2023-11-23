bbe8d8ab26594cbe965fd099e24f78d10091b484
Olga Liubimova <lov800@yandex.ru>
2023-11-23 11:11:22 +0300

    Настроить обработку CORS (7)

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,15 +12,13 @@ module.exports.cors = (req, res, next) => {
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  console.log(origin);
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
     }
+    return res.end();
   }
   return next();
 };
